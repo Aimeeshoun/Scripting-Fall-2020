9b5c917ab13ea1ae1a0b1ae4155b99e833efc912
Aimee <10893245@my.uvu.edu>
2020-08-25 10:13:13 -0600

    Scripting if statements

---
+++ scripting/Project 1 Scripting Fall 2020/Assets/ifstatement.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class ifstatement : MonoBehaviour
+{
+
+    public bool canrun;
+    public string password;
+
+   
+    void Start()
+    {
+        if (canrun)
+        {
+            Debug.Log("is correct!");
+        }
+       else {
+            Debug.Log("is not correct!");
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
