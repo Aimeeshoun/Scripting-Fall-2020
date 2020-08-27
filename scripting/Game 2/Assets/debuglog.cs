c29b9c4d20840e492e1368ed7b3bb7baa79a74d5
Aimee <10893245@my.uvu.edu>
2020-08-27 13:56:02 -0600

    Debug Log

---
+++ scripting/Game 2/Assets/debuglog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debuglog : MonoBehaviour
+{
+   
+    void Update()
+    {
+        Debug.Log("Game is on!");
+    }
+}
