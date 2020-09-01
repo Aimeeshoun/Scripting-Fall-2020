5cf5ea8c32c1961f1e590d454a8dd8702dc2b8f3
Aimee <10893245@my.uvu.edu>
2020-09-01 16:50:59 -0600

    Movement After class practice

--- scripting/Game 2/Assets/movement.cs
+++ scripting/Game 2/Assets/movement.cs
@@ -8,18 +8,32 @@ public class movement : MonoBehaviour
     private Vector3 movement1;
     public float gravity = -9.81f;
     public float moveSpeed = 3f;
-
+    public float jumpForce = 10f;
+    public int jumpCountMax;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
-            
+
     }
 
     void Update()
     {
-        movement1.y = gravity;
-        controller.Move(movement1*Time.deltaTime);
-        movement1.x = Input.GetAxis("Vertical");  
-    }
-}
+        { movement1.x *= -moveSpeed; }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement1.y = jumpForce;
+        }
+        if (controller.isGrounded)
+        {
+            movement1.y = 0;
+        }
+        else
+        {
+            movement1.y -= gravity;
+        }
+        controller.Move(movement1 * Time.deltaTime);
+
+    } }
