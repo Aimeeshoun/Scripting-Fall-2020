fbe2c3753a93f409698f02b93176ea52e1a61bb3
Aimee <10893245@my.uvu.edu>
2020-09-15 19:08:40 -0600

    ApplyForceScriptSept15

--- scripting/game3/Assets/sept 14 class scripts/Instaciate.cs
+++ scripting/game3/Assets/sept 14 class scripts/Instaciate.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Instaciate : MonoBehaviour
 {
     public GameObject prefab;
-    private void Start()
+    public UnityEvent Instanciating;
+
+    //Make a method to call the Instance Method
+
+    public void Instance()
     {
         var location = transform.position;
         var rotationDirection = new Vector3(0f, 45f, 0f);
         Instantiate(prefab, location, Quaternion.Euler(rotationDirection));
-
     }
+  
 }
---
+++ scripting/game3/Assets/sept15scrpts/ApplyForce1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce1 : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public float force = 30f;
+
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force, 0, 0);
+        //forceDirection needs to be based on Player rotation (hint SO)
+        rBody.AddRelativeForce(forceDirection);
+    }
+
+}
