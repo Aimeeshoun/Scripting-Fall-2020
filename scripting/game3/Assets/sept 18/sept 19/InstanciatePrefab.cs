144fadcd0b26749bc34ee3e8fa6fe1a092c5f565
Aimee <10893245@my.uvu.edu>
2020-09-28 14:09:05 -0600

    bullet counter, bullet instanciate script sept 28th

--- scripting/game3/Assets/sept 18/sept 19/InstanciatePrefab.cs
+++ scripting/game3/Assets/sept 18/sept 19/InstanciatePrefab.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class InstanciatePrefab : MonoBehaviour
 {
 
-    public GameObject myPrefab;
-
-
-    void Update()
+    
+    public float bulletSpeed = 10;
+    public Rigidbody bullet1;
+    void Fire1()
+    {
+        Rigidbody bullet = (Rigidbody) Instantiate(bullet1, transform.position, transform.rotation);
+        bullet.velocity = transform.forward * bulletSpeed;
+    }
+        
+        void Update()
     { if (Input.GetKeyDown(KeyCode.B))
             {
-            Instantiate(myPrefab, transform.position, Quaternion.identity);
+            Fire1();
 
         }
     }
