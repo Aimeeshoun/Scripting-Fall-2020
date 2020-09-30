dd2c095270a99d11c93bfb127aa7f8d2599befd9
Aimee <10893245@my.uvu.edu>
2020-09-30 16:19:23 -0600

    Movement Script sept 30th

---
+++ scripting/Character Movement/Assets/sept 30/scripts/movementScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    public Rigidbody myRigid;
+    public float normalSpeed;
+    
+    
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Vector3 theMovement = new Vector3(horizontalAxis, 0, verticalAxis) * normalSpeed * Time.deltaTime;
+        myRigid.MovePosition(transform.position + theMovement);
+    }
+}
