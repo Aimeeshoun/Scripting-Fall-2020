284e88440ce7c48873c73095fc80dcca550e3194
Aimee <10893245@my.uvu.edu>
2020-10-12 17:48:55 -0600

    OntriggerEnter Mesh Renderer/Material Change Oct 12

---
+++ scripting/Final Game/Assets/MartyOnTriggerEnter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponentTypeofMeshRenderer]
+public class MartyOnTriggerEnter : MonoBehaviour
+{
+
+    public Start()
+    {
+
+    }
+   public Ontriggerenter()
+    {
+        GetComponent<MeshRenderer>;
+       
+    }
+}
